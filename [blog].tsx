ebddf1a2c045130461fec3267a0ecf2c2972acbe
Napat Srichan <napatsrichan2001@gmail.com>
2022-02-16 22:57:37 -0800

    feat: :sparkles: add header for code blocks
    add a header part for putting file name in code blocks

--- [blog].tsx
+++ [blog].tsx
@@ -1,15 +1,49 @@
 import Link from "next/link";
+import Head from "next/head";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
+import rehypeHighlight from "rehype-highlight";
 import { Footer, timeToString } from ".";
 import s from "./blog.module.scss";
+
+export const Code = (props: any) => {
+  return (
+    <pre>
+      {props.children.props.metastring ? (
+        <p>{props.children.props.metastring}</p>
+      ) : (
+        <></>
+      )}
+      <code
+        style={
+          props.children.props.metastring
+            ? {}
+            : { borderTopLeftRadius: "12px", borderTopRightRadius: "12px" }
+        }
+        className={props.children.props.className}
+      >
+        {props.children.props.children}
+      </code>
+    </pre>
+  );
+};
+
+export const components = {
+  pre: Code,
+};
 
 const Blog = (props: any) => {
   return (
     <div className={s.blog}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
+        />
+      </Head>
       <div className="blog-page">
         <div className="header">
           <h1>{props.meta.title}</h1>
@@ -20,7 +54,7 @@ const Blog = (props: any) => {
           </p>
           <hr />
         </div>
-        <MDXRemote {...props.mdx} />
+        <MDXRemote {...props.mdx} components={components} />
         <br />
         <p>
           <Link href="/b">
@@ -39,7 +73,10 @@ export const getStaticProps = async ({ params: { blog } }: any) => {
     "utf-8"
   );
   const { data: meta, content } = matter(markdownWithMeta);
-  const mdx = await serialize(content);
+  const mdx = await serialize(content, {
+    //@ts-ignore
+    mdxOptions: { rehypePlugins: [rehypeHighlight] },
+  });
 
   return {
     props: {
